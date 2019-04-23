e3afc9e478fe622ee012653fbbeeb2875b2127df
Diana Ngo <dianango@Dianas-MacBook-Pro.local>
2019-04-23 12:05:47 +0200

    a

--- Koldkrigsmuseum/museum-forside.js
+++ Koldkrigsmuseum/museum-forside.js
@@ -1,15 +1,26 @@
 window.addEventListener("load", sidenVises);
 
 
-var burgerknap = document.querySelector('.burgerknap');
-var mobilmenu = document.querySelector('nav');
-burgerknap.addEventListener("click", function () {
-	if (mobilmenu.style.display == 'none' || mobilmenu.style.display == '') {
-		mobilmenu.style.display = 'flex';
+function sidenVises() {
+	console.log("siden vises!");
+	// registrer klik på menu-knap
+	document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+	console.log("Toggle menu");
+	document.querySelector("#menu").classList.toggle("hidden");
+
+	let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+	if (erSkjult == true) {
+		//menuen er nu skjult - ændr menuknap til lll
+		document.querySelector("#menuknap").textContent = "☰";
 	} else {
-		mobilmenu.style.display = 'none';
+		//menuen er nu vist - ændr menuknap til x
+		document.querySelector("#menuknap").textContent = "☰"
 	}
-});
+}
 
 
 let statiske_sider = [];
