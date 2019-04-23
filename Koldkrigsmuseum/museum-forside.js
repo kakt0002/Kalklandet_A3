be221daedd070c83cbb04f3347a484ab7e0fbbf5
Diana Ngo <dianango@Dianas-MacBook-Pro.local>
2019-04-23 09:37:38 +0200

    ny

--- Koldkrigsmuseum/museum-forside.js
+++ Koldkrigsmuseum/museum-forside.js
@@ -23,11 +23,11 @@ function toggleMenu() {
 }
 
 
-let retter = [];
+let statiske_sider = [];
 document.addEventListener("DOMContentLoaded", hentJson);
 
 async function hentJson() {
-	const url = "http://mabe-kea.dk/F19/tema7/wp/wp-json/wp/v2/posts?per_page=50";
+	const url = "http://sophiasvane.dk/kea/07-cms/kalklandet_a3/wordpress/wp-json/wp/v2/statiske_sider/131";
 	const myJson = await fetch(url);
 	retter = await myJson.json();
 	console.log(myJson);
@@ -39,9 +39,15 @@ function visRetter() {
 	let temp = document.querySelector("template");
 
 	retter.forEach(ret => {
+
 		let klon = temp.cloneNode(true).content;
-		klon.querySelector("h2").innerHTML = ret.title.rendered;
-		klon.querySelector("div").innerHTML = ret.excerpt.rendered;
+		klon.querySelector(".titel").innerHTML = ret.title.rendered;
+		klon.querySelector(".beskrivelse").innerHTML = ret.content.rendered;
+		klon.querySelector(".pris").innerHTML = ret.pris;
+		klon.querySelector(".kategori").innerHTML = ret.kategori;
+		klon.querySelector(".foto").src = ret.billede.guid;
 		dest.appendChild(klon);
+
+
 	})
 }
